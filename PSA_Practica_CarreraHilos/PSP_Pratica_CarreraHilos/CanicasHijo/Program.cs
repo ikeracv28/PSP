e2834822d5df8243fa065671b70559c4debf5c30
iker acevedo <iker.acevedo24@campusfp.es>
2025-11-17 22:15:58 +0100

    subida de trabajo hilos

---
+++ PSA_Practica_CarreraHilos/PSP_Pratica_CarreraHilos/CanicasHijo/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace CanicasHijo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // El proceso padre me pasa 2 datos:
+            // args[0] = número de la pista
+            // args[1] = cuántas canicas (hilos) tiene esta pista
+            int pista = int.Parse(args[0]);
+            int numCanicas = int.Parse(args[1]);
+
+            // Array donde cada hilo guardará su tiempo
+            long[] tiempos = new long[numCanicas];
+
+            // Creamos un hilo por cada canica
+            Thread[] hilos = new Thread[numCanicas];
+
+            for (int i = 0; i < numCanicas; i++)
+            {
+                int idCanica = i + 1;
+                int indice = i;
+
+                hilos[i] = new Thread(() =>
+                {
+                    // Cronómetro para medir el tiempo de la canica
+                    Stopwatch crono = new Stopwatch();
+                    crono.Start();
+
+                    // La canica avanza hasta llegar a 100
+                    int posicion = 0;
+                    Random rnd = new Random(idCanica * 100);
+
+                    while (posicion < 100)
+                    {
+                        posicion += rnd.Next(1, 6); // avanza de 1 a 5
+                        Thread.Sleep(10); // simula trabajo
+                    }
+
+                    crono.Stop();
+
+                    // Guardamos el tiempo que tardó esta canica
+                    tiempos[indice] = crono.ElapsedMilliseconds;
+                });
+
+                hilos[i].Start();
+            }
+
+            // Esperamos a que todas las canicas terminen
+            foreach (var h in hilos) h.Join();
+
+            // Imprimimos resultados EN FORMATO SIMPLE para que lo lea el proceso padre
+            // Formato: pista;canica;tiempo
+            for (int i = 0; i < numCanicas; i++)
+            {
+                int idCanica = i + 1;
+                Console.WriteLine($"{pista};{idCanica};{tiempos[i]}");
+            }
+        }
+    }
+}
---
+++ PSA_Practica_CarreraHilos/PSP_Pratica_CarreraHilos/CanicasPadre/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace CanicasPadre
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Decimos cuántas pistas y cuántas canicas tiene cada pista
+            int numPistas = 3;
+            int canicasPorPista = 3;
+
+            // Archivo .exe del proyecto hijo
+            string exeHijo = @"C:\repositorio\PSA_Practica_CarreraHilos\PSP_Pratica_CarreraHilos\CanicasHijo\bin\Debug\net8.0\CanicasHijo.exe";
+
+            // Aquí guardaremos TODOS los resultados
+            List<Resultado> lista = new List<Resultado>();
+
+            for (int pista = 1; pista <= numPistas; pista++)
+            {
+                Console.WriteLine($"[Padre] Lanzando proceso hijo para la pista {pista}...");
+
+                // Cronómetro del proceso hijo
+                Stopwatch cronoHijo = new Stopwatch();
+                cronoHijo.Start();
+
+
+                // Preparamos los argumentos para el hijo
+                string argumentos = $"{pista} {canicasPorPista}";
+
+                ProcessStartInfo psi = new ProcessStartInfo();
+                psi.FileName = exeHijo;
+                psi.Arguments = argumentos;
+                psi.UseShellExecute = false;
+                psi.RedirectStandardOutput = true;
+
+                // Lanzamos el proceso hijo (la pista)
+                Process p = Process.Start(psi);
+
+                // Leemos lo que escribe el hijo
+                string salida = p.StandardOutput.ReadToEnd();
+                p.WaitForExit();
+
+
+                cronoHijo.Stop();
+                Console.WriteLine($"[Padre] Pista {pista} terminó en {cronoHijo.ElapsedMilliseconds} ms\n");
+
+                // Procesamos cada línea
+                string[] lineas = salida.Split('\n');
+                foreach (string l in lineas)
+                {
+                    if (string.IsNullOrWhiteSpace(l)) continue;
+
+                    // Formato: pista;canica;tiempo
+                    string[] partes = l.Split(';');
+
+                    lista.Add(new Resultado
+                    {
+                        Pista = int.Parse(partes[0]),
+                        Canica = int.Parse(partes[1]),
+                        Tiempo = long.Parse(partes[2])
+                    });
+                }
+            }
+
+            // Ordenamos por tiempo
+            var ranking = lista.OrderBy(r => r.Tiempo).ToList();
+
+            Console.WriteLine("\n=== CLASIFICACIÓN GLOBAL ===\n");
+
+            int pos = 1;
+            foreach (var r in ranking)
+            {
+                Console.WriteLine($"{pos}. Pista {r.Pista} - Canica {r.Canica} - Tiempo: {r.Tiempo} ms");
+                pos++;
+            }
+
+            Console.WriteLine("\nTerminado.");
+        }
+    }
+
+    class Resultado
+    {
+        public int Pista { get; set; }
+        public int Canica { get; set; }
+        public long Tiempo { get; set; }
+    }
+}
